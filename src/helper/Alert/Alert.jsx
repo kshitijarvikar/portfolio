5b9925ebf8392cb8357b2bed0a71b95dedc5fb20
Kshitij Arvikar <kshitijuarvikar849@gmail.com>
2023-10-22 23:30:30 +0530

    updated files

--- src/helper/Alert/Alert.jsx
+++ src/helper/Alert/Alert.jsx
@@ -9,7 +9,8 @@ const Alert = () => {
         <span className="text1"> This portfolio is still under development. | </span>
         <span className="text1"> You can Contribute to this website  | </span>
         <span className="text1"> This is not mobile responsive yet. | </span>
-        <span className="text1"> You can Contribute to this website. | </span>
+        <span className="text1"> This portfolio is still under development. | </span>
+        <span className="text1"> You can Contribute to this website  | </span>
         <span className="text1"> This is not mobile responsive yet. | </span>
       </Marquee>
     </div>
